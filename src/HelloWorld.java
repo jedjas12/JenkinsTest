821d020deec7324ff47686bcf11efe5001e0faf5
jedjasie12 <x-wing12>
2017-07-16 01:10:54 -0400

    Initial commit. HelloWorld test for Jenkins usage

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * Created by jasie on 7/14/17.
+ */
+import sun.util.resources.cldr.pl.TimeZoneNames_pl;
+
+import java.math.*;
+import java.time.LocalTime;
+public class HelloWorld {
+
+        public static void main(String[] args)
+        {
+            System.out.println(Math.addExact(10000, 34563456));
+            System.out.println("Hello, World!");
+            System.out.println("I'm in");
+        }
+    }
+
+
